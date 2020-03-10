0c99ff606919ae403d2c984ae38a9aa7d17bd397
Alim Yusif-zada <yusifzaj@gmail.com>
2020-03-10 07:24:20 +0400

    Add files via upload

--- block.py
+++ block.py
@@ -1,43 +1,47 @@
-## version 0.1 Feb-24,2020
-## AMPL logic block
+## version 0.1 Feb-24,2020 BAKU ABB ARMOR AY
+## Advant Controllers AAX files parsing and comparision
 
-EQ=' = '
-NEQ=' ! '
-ERR1='ERROR:check types'
+NEQ=' <not equal> '
 NONE='NONE'
-_tab=16
+_tab=30
 
 ##-----------------------------------------------------------------------
-    
-class Block:
+
+class block:
 
     def __init__(self,address='PC',name='dummy',extra='()'):
+        "create logic block"
         self.pins={} ## block pins (keys) and connections {pin:connection,..}
         self.name=name ## block name
         self.address=address ## PC##.##.##...
         self.extra=extra ## everything in the brackets
         return
 
-    def gpin(self,pin): ## get string value of the pin
+    def getpin(self,pin): ##
+        "get string value of the pin"
         if pin in self.pins.keys():
             return str(self.pins[pin])
         else:
             return NONE
-    
-    def apin(self,pin,value): ## add pin
+
+    def addpin(self,pin,value):
+        "create a pin with a value"
         if pin in self.pins.keys():
+            print('pin %s already exist'%pin)
             return False
         self.pins[pin]=value
         return True
 
-    def __str__(self):  ## print block
+    def __str__(self):
+        "string representation of the block"
         s=''
         for k in self.pins.keys():
-            s=s+'\t'+str(k).ljust(_tab)+self.gpin(k)+'\n'
+            s=s+'\t'+str(k).ljust(_tab)+self.getpin(k)+'\n'
         return "%s\t%s %s\n%s"%(self.address,self.name,self.extra,s)
 
     def __eq__(self,other):
-        if isinstance(other,Block):
+        "compare blocks like blokA==blockB"
+        if isinstance(other,block):
             if other.address==self.address:
                 if other.name==self.name:
                     if other.extra==self.extra:
@@ -46,35 +50,36 @@ class Block:
         return False
 
     def cmp(self,other):
+        "compare blocks and return reprt about differences"
         s=''
-        if isinstance(other,Block):
+        if isinstance(other,block):
             slist=list(self.pins.keys())
             olist=list(other.pins.keys())
+            if self.name!=other.name:
+                s=s+self.name+NEQ+other.name+'\n'
+            if self.extra!=other.extra:
+                s=s+self.extra+NEQ+other.extra+'\n'
             if len(slist)<len(olist): ## slist should always bigger
                 slist=list(other.pins.keys())
-                olist=list(self.pins.keys())    
+                olist=list(self.pins.keys())
             for k in slist:
                 if k in olist:
-                    if self.pins[k]==other.pins[k]:
-                        i=EQ
-                    else:
-                        i=NEQ
-                else:
-                    i=NEQ
-                s=s+'\t'+str(k).ljust(_tab)+self.gpin(k).ljust(_tab)+i.ljust(_tab)+other.gpin(k)+'\n'
+                    if self.pins[k]!=other.pins[k]:
+                        s=s+'\t'+str(k).ljust(_tab)+self.getpin(k).ljust(_tab)+NEQ+other.getpin(k).rjust(_tab)+'\n'
             for k in olist:
                 if k not in slist:
-                    s=s+'\t'+str(k).ljust(_tab)+self.gpin(k).ljust(_tab)+NEQ.ljust(_tab)+other.gpin(k)+'\n'               
+                    s=s+'\t'+str(k).ljust(_tab)+self.getpin(k).ljust(_tab)+NEQ+other.getpin(k).rjust(_tab)+'\n'
         return s
 
-    
+
 class aax:
-    
+
     def __init__(self,fname):
-        self.el={}
-        self.fname=fname
-        status=0
-        self.lines=None
+        "parsing aax file"
+        self.el={} # logic elements (blocks)
+        self.fname=fname # aax file name
+        status=0 # used for parsing
+        self.lines=None # lines collection from aax file
         try: # open aax file
             file=open(self.fname,'r')
             self.lines=file.readlines()
@@ -82,17 +87,17 @@ class aax:
         except:
             print('error reading file:'+self.fname)
             return
-        address='' # address of the block 
+        address='' # address of the block
         blkname='' # name of the block  or  pin
         pinname=''
         extra=''
-        pinval='' 
-        lpinval=[] # for multiple connections
-        elcnt=0 # number of elements in the line       
-## AMPLE parsing logic from here        
+        pinval=''
+        lpinval=[] # list of the values for multiple connected pins
+        elcnt=0 # number of elements in the line
+## AMPLE parsing logic from here
         for  L in self.lines:
             line=L.split()
-            elcnt=len(line)  # count the elements in the line             
+            elcnt=len(line)  # count the elements in the line
             if elcnt>0 and line[0][:2]=='PC' and line[0][2:3].isdigit(): #start of the logic block
                 address=line[0] # get address
                 status=1 #    block mark
@@ -104,55 +109,61 @@ class aax:
                     extra=line[2] # get extra params
                 else:
                     extra=''
-                self.el[address]=Block(address,blkname,extra) # create logic block obj
-            if elcnt>0 and line[0][:1]==':': # start of the pin definition 
+                self.el[address]=block(address,blkname,extra) # create logic block obj
+            if elcnt>0 and line[0][:1]==':': # start of the pin definition
                 pinname=line[0]# get pin name
                 status=2 #  pin mark
-                if elcnt>2: # if there are spaces 
+                if elcnt>2: # if there are spaces in the pin value
                     st=''
                     for i in range(elcnt):
                         if i>0:
                             st+=line[i]+' ' # put all of them in to one string
                     line.clear()
-                    line=[pinname,st]                      
+                    line=[pinname,st]
                 if elcnt>1:
                     pinval=line[1] #get pin value
                 if pinval[-1:]==',':# another value at the next line
                     status=3
                     lpinval.append(pinval[:-1])
                 else:
-                    self.el[address].apin(pinname,pinval)
-            if elcnt==1 and status==3:
+                    self.el[address].addpin(pinname,pinval) # last value for the pin
+                    status=0
+            if elcnt==1 and status==3: # one of the values for the pin - add it to the list
                 pinval=line[0]
-                if pinval[-1:]==',':
+                if pinval[-1:]==',': # there are still another value at the next line
                     lpinval.append(pinval[:-1])
-                else:
+                else: # this is a last value for the pin
                     lpinval.append(pinval)
-                    self.el[address].apin(pinname,lpinval)
+                    self.el[address].addpin(pinname,lpinval) # add list to the pin
+                    lpinval=[]
                     status=0
 
     def count(self,blkname='dummy'):
+        "count logic blocks in aax file"
         counter=0
         for k in self.el.keys():
             if self.el[k].name==blkname:
                 counter+=1
         return counter
 
-            
-## simple format
-## (* commentary notes *)
-    
-## BEGIN PC##
-    
-## HEADER
-## text
+    def cmp(self,other):
+        "compare aax files and return report"
+        s=''
+        if isinstance(other,aax):
+            skeys=self.el.keys()
+            okeys=other.el.keys()
 
-## PC##     PCPGM ()
+            if len(skeys)!=len(okeys):
+                s=s+'\nNumers of logic blocks at %s =%d differnet from %s =%d\n'%(self.fname,len(self.el.keys()),other.fname,len(other.el.keys()))
+            for key in self.el.keys():
+                if key in other.el.keys():
+                    if self.el[key]!=other.el[key]:
+                        s=s+'\nConflict at %s\n'%(key)+str(self.el[key].cmp(other.el[key]))
+                else:
+                    s=s+'\nAddress %s not found at %s but exist at %s\n'%(key,other.fname,self.fname)+str(self.el[key])
+            for key in other.el.keys():
+                if key not in self.el.keys():
+                    s=s+'\nAddress %s not found at %s but exist at %s\n'%(key,self.fname,other.fname)+str(other.el[key])
+        return s
 
-## address       name   extra
-## [PC##.##...] [name] <(parameters)>
-##    list of pins and connections/values
-##    [:][pin]   [value]
-    
-## END PC##
 
