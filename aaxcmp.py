ac5e6c22dea69d9219873bd14c605ca64de2d7e5
AlimYusifzada <yusifzaj@gmail.com>
2020-03-17 23:32:35 +0400

    minor changes

--- aaxcmp.py
+++ aaxcmp.py
@@ -7,13 +7,11 @@ print('Mar,2020,AY AMPL logic block compare')
 if len(sys.argv)<3:
     print('not enough arguments')
     print('from command line prompt run: aaxcmp [file1.aax] [file2.aax]')
-    exit()
-try:
-    file1=aax(sys.argv[1])
-    file2=aax(sys.argv[2])
-    print(file1.cmp(file2))
-except:
-    print('fatal error')
-    exit()
+
+print('compare %s vs %s'%(sys.argv[1],sys.argv[2]))
+file1=aax(sys.argv[1])
+file2=aax(sys.argv[2])
+print(file1.cmp(file2))
+
 
 
