20edc96c2fe1e49b16ff2ec0c52ac441cb6a49d1
Alim Yusif-zada <yusifzaj@gmail.com>
2020-04-12 09:07:36 +0400

    rev 0.5 GUI mods

--- aaxcmp.py
+++ aaxcmp.py
@@ -14,25 +14,46 @@ wwidth=120
 options=()
 files=()
 
+rowINFO=0
+rowBUTTONS=1
+rowBefore=2
+rowAfter=3
+rowOUTPUT=4
+
 class mainGUI:
 
     def __init__(self,root):
         self.root=root
 
-        Label(text='gui interface for aaxcmp rev %s Mar/2020'%rev).grid(row=0,column=10,sticky='E'+'W')
-        Label(text='AAX file before:').grid(row=1,column=9,sticky='E'+'W')
-        self.FBefore=Entry(root,width=wwidth)
-        self.FBefore.grid(row=1,column=10,sticky='W'+'E')
-        Label(text='AAX file after:').grid(row=2,column=9,sticky='E'+'W')
-        self.FAfter=Entry(root,width=wwidth)
-        self.FAfter.grid(row=2,column=10,sticky='W'+'E')
-        self.TagEdit=Entry(root)
-        self.TagEdit.grid(row=2,column=1,sticky='W'+'E')
+## LABELS
+        root.title('AAXCMP GUI rev %s Mar/2020'%rev)
+        ##Label(text='gui interface for aaxcmp rev %s Mar/2020'%rev).grid(row=rowINFO,column=10,sticky='E'+'W')
+        Label(text='AAX file before:').grid(row=rowBefore,column=9,sticky='E'+'W')
+        Label(text='AAX file after:').grid(row=rowAfter,column=9,sticky='E'+'W')
 
-        self.cmpBTN=Button(root,text='cmp',command=self.icompare).grid(row=1,column=0)
-        self.voidBTN=Button(root,text='xref tag below',command=self.vpins).grid(row=1,column=1)
+## OUTPUT
         self.cmpOutput=STX.ScrolledText(root)
-        self.cmpOutput.grid(row=3,column=1,sticky='N'+'S'+'w'+'E',columnspan=10)
+        self.cmpOutput.grid(row=rowOUTPUT,column=0,sticky='N'+'S'+'w'+'E',columnspan=11)
+
+## ENTRIES
+
+## AAX file entry - BEFORE
+        self.FBefore=Entry(root,width=wwidth)
+        self.FBefore.grid(row=rowBefore,column=10,sticky='W'+'E')
+
+## AAX file entry - AFTER
+        self.FAfter=Entry(root,width=wwidth)
+        self.FAfter.grid(row=rowAfter,column=10,sticky='W'+'E')
+
+## TAG NAME entry - cross reference
+        self.TagEdit=Entry(root)
+        self.TagEdit.grid(row=rowBefore,column=1,columnspan=2,sticky='W'+'E')
+
+## BUTTONS
+## button COMPARE
+        self.cmpBTN=Button(root,text='Compare',command=self.icompare).grid(row=rowBUTTONS,column=1)
+## button CROSS REFERENCE
+        self.voidBTN=Button(root,text='Cross reference',command=self.vpins).grid(row=rowBUTTONS,column=2)
 
     def icompare(self):
         fA=aax(self.FBefore.get())
@@ -53,7 +74,8 @@ class mainGUI:
 def aaxgui():
     mainwin=Tk()
     win=mainGUI(mainwin)
-    win.title='aaxcmp with GUI rev%s'%rev
+    mainwin.grid_rowconfigure(rowOUTPUT,weight=1)
+    mainwin.grid_columnconfigure(0,weight=1)
     mainwin.mainloop()
     pass
 
@@ -62,12 +84,10 @@ def help():
     print('options could be:')
     print(' -i compare logic blocs')
     print(' -l compare line by line')
-##    print(' -L compare line by conflicts selected (linux terminal tested)')
+    print(' -L compare line by conflicts selected (linux terminal tested)')
     print(' -rTAG_NAME cross reference of TAG_NAME at file1.aax and file2.aax')
     print('            NOTE: spaces are not applicable')
     print(' -v search for unconnected pins')
-##    print(' -s print some statistics (dont use - in development)')
-##    print(' -w start GUI (dont use - in development)')
     print(' -h print this help')
     print('position of the options keys in the command line, determine the sequence of the output')
     print('AAX files names location in the command line are not fixed but both should be present')
@@ -107,14 +127,14 @@ for op in options:
         print(fileOne.statout())
         print('File %s'%fileTwo.fname)
         print(fileTwo.statout())
-##    if op=='-L':
-##        d=dif.Differ()
-##        cmpres=d.compare(fileOne.Lines,fileTwo.Lines)
-##        for i in cmpres:
-##            if i[0]=='-' or i[0]=='+' or i[0]=='?':
-##                print(CSELECTED+i+CEND,end='')
-##            else:
-##                print(i,end='')
+    if op=='-L':
+        d=dif.Differ()
+        cmpres=d.compare(fileOne.Lines,fileTwo.Lines)
+        for i in cmpres:
+            if i[0]=='-' or i[0]=='+' or i[0]=='?':
+                print(CSELECTED+i+CEND,end='')
+            else:
+                print(i,end='')
     if op=='-l':
         d=dif.Differ()
         cmpres=d.compare(fileOne.Lines,fileTwo.Lines)
