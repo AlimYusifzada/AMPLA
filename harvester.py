d45a3298b9b9360e315738109b2cdaadd4ed1b74
AlimYusifzada <yusifzaj@gmail.com>
2021-07-18 11:42:49 +0400

    make source more readable

--- harvester.py
+++ harvester.py
@@ -1,4 +1,4 @@
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # Name:        module1
 # Purpose:
 #
@@ -7,59 +7,65 @@
 # Created:     05/05/2020
 # Copyright:   (c) Alim 2020
 # Licence:
-#-------------------------------------------------------------------------------
-from tkinter import Tk,Button,Frame,Label,Listbox,filedialog
+# -------------------------------------------------------------------------------
+from tkinter import Tk, Button, Frame, Label, Listbox, filedialog
 from shutil import copy
 
 
-mainFrame=Tk()
-btnROW=0
-lstROW=1
-fileList=()
+mainFrame = Tk()
+btnROW = 0
+lstROW = 1
+fileList = ()
 
 
 def addfile():
     global fileList
-    fn=filedialog.askopenfile(initialdir='/',
-                            title='select files to harvest')
-    if fn!=None:
-        fileList+=(fn.name,)
-        print('%s\tadded to the list'%fn.name)
+    fn = filedialog.askopenfile(initialdir='/',
+                                title='select files to harvest')
+    if fn != None:
+        fileList += (fn.name,)
+        print('%s\tadded to the list' % fn.name)
     pass
+
 
 def start():
     global fileList
     mainFrame.iconify()
-    dest=filedialog.askdirectory(initialdir='/',
-                            title='select destination for the files')
+    dest = filedialog.askdirectory(initialdir='/',
+                                   title='select destination for the files')
     print("\n!!!!! COPY IN PROGRESS!, PLEASE DO NOT TERMINATE !!!!!")
     for f in fileList:
-        copy(f,dest)
+        copy(f, dest)
     print('DONE')
     pass
 
+
 def clearlist():
     global fileList
-    fileList=()
-    print('\nfile list empty\n',fileList)
+    fileList = ()
+    print('\nfile list empty\n', fileList)
 
-addBTN=Button(text='add file',command=addfile)
-exeBTN=Button(text='start',command=start)
-clsBTN=Button(text='clear',command=clearlist)
+
+addBTN = Button(text='add file', command=addfile)
+exeBTN = Button(text='start', command=start)
+clsBTN = Button(text='clear', command=clearlist)
+
 
 def framesetup():
-    mainFrame.grid_columnconfigure(0,weight=1)
-    mainFrame.grid_rowconfigure(0,weight=1)
+    mainFrame.grid_columnconfigure(0, weight=1)
+    mainFrame.grid_rowconfigure(0, weight=1)
     mainFrame.title("harvester")
-    addBTN.grid(row=btnROW,column=0,sticky='W'+'E')
-    clsBTN.grid(row=btnROW,column=1,sticky='W'+'E')
-    exeBTN.grid(row=btnROW,column=2,sticky='W'+'E')
+    addBTN.grid(row=btnROW, column=0, sticky='W'+'E')
+    clsBTN.grid(row=btnROW, column=1, sticky='W'+'E')
+    exeBTN.grid(row=btnROW, column=2, sticky='W'+'E')
     pass
+
 
 def main():
     framesetup()
     mainFrame.mainloop()
     pass
+
 
 if __name__ == '__main__':
     main()
