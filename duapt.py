53dd746143d7b5fcd82ac536e36623710747a9b5
Alim Yusif-zada <yusifzaj@gmail.com>
2026-04-08 14:06:03 +0400

    Refactor duapt.py for improved readability and style

--- duapt.py
+++ duapt.py
@@ -1,134 +1,138 @@
+# Refactored on 08/04/2026 13:51
 import os
 from datetime import datetime as dt
+
 import xlwt
 
-duapt_rev='0.22.02.08'
+DUAPT_REV = '0.22.02.08'
 
-duapthelp='''
+DUAPT_HELP = '''
 Search for *.LG files at the project directory.
 Transfer all of them to the convenient folder.
 Upon completion look for duap_timing.xls.
 
 Enter the path to the LG files:'''
 
-nodeslist = []
 
-def getDUAPtime(logfile):
-
+def get_duap_time(logfile):
     try:
-        with open(logfile, 'rb') as lf:  # open and read LG file
+        with open(logfile, 'rb') as lf:
             rawdata = lf.read()
-    except:
-        print('cant find file '+logfile)
-        return
+    except Exception:
+        print(f"cant find file {logfile}")
+        return None
 
-    logtext = str(rawdata)  # convert bytes to string
+    logtext = str(rawdata)
     nodenumidx = logtext.find('Node')
-    startimeidx = logtext.find('TIME')  # find position of the first (start)
-    stoptimeidx = logtext.rfind('TIME')  # and finish time
-    bkpsizeidx = logtext.find('DUMP SIZE') #DUAP size in bytes here
+    startimeidx = logtext.find('TIME')
+    stoptimeidx = logtext.rfind('TIME')
+    bkpsizeidx = logtext.find('DUMP SIZE')
 
-    # get DUAP backup size in bytes
-    bkpsize=logtext[bkpsizeidx+11:bkpsizeidx+15]
-    nodenum = logtext[nodenumidx+10:nodenumidx+12]
-    # get HH:MM:SS DUAP time start
-    sstartime = logtext[startimeidx+18:startimeidx+26]
-    # get HH:MM:SS DUAP time finish
-    sstoptime = logtext[stoptimeidx+18:stoptimeidx+26]
+    bkpsize = logtext[bkpsizeidx + 11:bkpsizeidx + 15]
+    nodenum = logtext[nodenumidx + 10:nodenumidx + 12]
+    sstartime = logtext[startimeidx + 18:startimeidx + 26]
+    sstoptime = logtext[stoptimeidx + 18:stoptimeidx + 26]
 
-    # convert text to datetime format
     startime = dt.strptime(sstartime, '%H:%M:%S')
     stoptime = dt.strptime(sstoptime, '%H:%M:%S')
-    difftime = stoptime-startime  # calculate DUAP duration
-    bs=(int(bkpsize))/difftime.total_seconds()
-    speed=str("%.2f" % bs) # speed b/s
+    difftime = stoptime - startime
+    
+    bs = int(bkpsize) / difftime.total_seconds()
+    speed = f"{bs:.2f}"
 
-    return (nodenum, startime, stoptime, difftime, bkpsize, speed)
+    return nodenum, startime, stoptime, difftime, bkpsize, speed
 
 
-def getfiles(dir):
-    files = 0
+def get_files(directory):
+    files = []
     try:
-        w = os.walk(dir)
-        for r, d, f in w:
-            files = f
-    except:
-        print('cant read directory:'+dir)
+        for _, _, filenames in os.walk(directory):
+            files = filenames
+    except Exception:
+        print(f"cant read directory:{directory}")
 
     return files
 
-def xlsreport(dir,nodeslist):
 
+def xls_report(directory, nodes_list):
+    node_a_col = 0
+    node_b_col = 1
+    size_col = 2
+    start_a_col = 3
+    stop_a_col = 4
+    speed_a_col = 5
+    start_b_col = 6
+    stop_b_col = 7
+    speed_b_col = 8
 
-    nodeA_col=0
-    nodeB_col=1
-    size_col=2
-    startA_col=3
-    stopA_col=4
-    speedA_col=5
-    startB_col=6
-    stopB_col=7
-    speedB_col=8
+    prev_node_num = 0
+    rec_cnt = 1
+    new_pair = False
 
-    prevNodeNum=0
-    rec_cnt=1 # start from this row
+    workbook = xlwt.Workbook()
+    sheet = workbook.add_sheet("Duap timimng")
+    
+    sheet.write(0, node_a_col, 'Side-A node')
+    sheet.write(0, node_b_col, 'Side-B node')
+    sheet.write(0, start_a_col, 'Start time A')
+    sheet.write(0, start_b_col, 'Start time B')
+    sheet.write(0, stop_a_col, 'Stop time A')
+    sheet.write(0, stop_b_col, 'Stop time B')
+    sheet.write(0, size_col, 'Size kb')
+    sheet.write(0, speed_a_col, 'Speed A kb/s')
+    sheet.write(0, speed_b_col, 'Speed B kb/s')
 
-    new_pair=False
-
-    xlsreport=xlwt.Workbook()
-    duaptsheet=xlsreport.add_sheet("Duap timimng") #add date to the sheet name
-    duaptsheet.write(0,nodeA_col,'Side-A node')
-    duaptsheet.write(0,nodeB_col,'Side-B node')
-    duaptsheet.write(0,startA_col,'Start time A')
-    duaptsheet.write(0,startB_col,'Start time B')
-    duaptsheet.write(0,stopA_col,'Stop time A')
-    duaptsheet.write(0,stopB_col,'Stop time B')
-    duaptsheet.write(0,size_col,'Size kb')
-    duaptsheet.write(0,speedA_col,'Speed A kb/s')
-    duaptsheet.write(0,speedB_col,'Speed B kb/s')
-
-    for rec in nodeslist:
-        if (prevNodeNum+1)==int(rec[0]) and new_pair:
-            duaptsheet.write(rec_cnt,nodeB_col,rec[0])
-            duaptsheet.write(rec_cnt,startB_col,rec[1])
-            duaptsheet.write(rec_cnt,stopB_col,rec[2])
-            duaptsheet.write(rec_cnt,speedB_col,rec[5])
-            rec_cnt+=1
-            new_pair=False
+    for rec in nodes_list:
+        if (prev_node_num + 1) == int(rec[0]) and new_pair:
+            sheet.write(rec_cnt, node_b_col, rec[0])
+            sheet.write(rec_cnt, start_b_col, rec[1])
+            sheet.write(rec_cnt, stop_b_col, rec[2])
+            sheet.write(rec_cnt, speed_b_col, rec[5])
+            rec_cnt += 1
+            new_pair = False
         else:
-            duaptsheet.write(rec_cnt,nodeA_col,rec[0])
-            duaptsheet.write(rec_cnt,startA_col,rec[1])
-            duaptsheet.write(rec_cnt,stopA_col,rec[2])
-            duaptsheet.write(rec_cnt,size_col,rec[4])
-            duaptsheet.write(rec_cnt,speedA_col,rec[5])
-            new_pair=True
-        prevNodeNum=int(rec[0])
+            sheet.write(rec_cnt, node_a_col, rec[0])
+            sheet.write(rec_cnt, start_a_col, rec[1])
+            sheet.write(rec_cnt, stop_a_col, rec[2])
+            sheet.write(rec_cnt, size_col, rec[4])
+            sheet.write(rec_cnt, speed_a_col, rec[5])
+            new_pair = True
+        
+        prev_node_num = int(rec[0])
 
-    xlsreport.save(dir+'\\Duap_Timing.xls')
-    pass
+    workbook.save(os.path.join(directory, 'Duap_Timing.xls'))
 
-def duaptreport(dir):
-    rowcnt=0
-    for duaplog in getfiles(dir):
-        if('LG' not in duaplog[-2:]):
+
+def duapt_report(directory):
+    nodes_list = []
+    
+    for duaplog in get_files(directory):
+        if 'LG' not in duaplog[-2:]:
             continue
-        dumptime = getDUAPtime(dir+'\\'+duaplog)
-        num = dumptime[0]  # node number
-        stt = str(dumptime[1])[-8:]  #  start time
-        stp = str(dumptime[2])[-8:]  #  stop time
-        dur = dumptime[3]  #  duration
-        siz = dumptime[4] #  size
-        spd = dumptime[5] #  speed
-        nodeslist.append((num, stt, stp, dur, siz, spd))
-    xlsreport(dir,nodeslist)
-    ##input('\n\tpress ENTER to exit')
+            
+        dumptime = get_duap_time(os.path.join(directory, duaplog))
+        if not dumptime:
+            continue
+            
+        num = dumptime[0]
+        stt = str(dumptime[1])[-8:]
+        stp = str(dumptime[2])[-8:]
+        dur = dumptime[3]
+        siz = dumptime[4]
+        spd = dumptime[5]
+        
+        nodes_list.append((num, stt, stp, dur, siz, spd))
+        
+    xls_report(directory, nodes_list)
+
 
 def main():
-    print('='*60)
+    print('=' * 60)
     print('DUAP time collector')
-    dir = input(duapthelp)
-    print('-'*60)
-    duaptreport(dir)
+    directory = input(DUAPT_HELP)
+    print('-' * 60)
+    duapt_report(directory)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
